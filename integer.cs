72387395b18a9c681c8f6aa47c745ea1d4cb44bc
deepakkaleru <deepakkaleru@gmail.com>
2025-02-25 17:16:45 +0530

    integer practice code

---
+++ integer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+//namespace integerfloat
+//{
+//class Program
+//{
+//static void Main(string[] args)
+WorkWithIntegers();
+void WorkWithIntegers()
+{
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+    int d = a - c;
+    int e = a * c;
+    int f = a / b;
+    int g = a * b - c * d / f;
+    Console.WriteLine(c);
+
+    Console.WriteLine(d);
+    
+    Console.WriteLine(e);
+    
+    Console.WriteLine(f);
+    Console.WriteLine(g);
+}
