2766bb783e971611516106527cf9cb5d3c7e5049
deepakkaleru <deepakkaleru@gmail.com>
2025-03-01 12:55:07 +0530

    fibonacci series practice

---
+++ Fibonacci-series.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+  static void Main()
+    {
+        int n = 25;
+        int[] fibonacciSeries = new int[n];
+        fibonacciSeries[0] = 0;
+        fibonacciSeries[1] = 1;
+        for (int i = 2; i < n; i++)
+        {
+            fibonacciSeries[i] = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+        }
+        Console.WriteLine("Fibonacci Series:");
+        foreach (int num in fibonacciSeries)
+        {
+            Console.Write(num +"    ");
+        }
+    }
+}
+
