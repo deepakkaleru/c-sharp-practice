5547842b3c8ae93960dc1145cd648b2df69b71f0
deepakkaleru <deepakkaleru@gmail.com>
2025-03-16 10:53:22 +0530

    generic method example

---
+++ generic-method.cs
@@ -0,0 +1,17 @@
+﻿public class GenericMethods
+{
+    public void Display<T>(T value)
+    {
+        Console.WriteLine($"Value: {value}, Type: {typeof(T)}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        GenericMethods gm = new GenericMethods();
+        gm.Display(123);     
+        gm.Display("Hello");   
+        gm.Display(3.14);      
+    }
+}
