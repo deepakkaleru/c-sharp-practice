a2a20c36a037c764ea6bfbb870222f65002cf79d
deepakkaleru <deepakkaleru@gmail.com>
2025-03-16 09:33:07 +0530

    interface example

---
+++ interface.cs
@@ -0,0 +1,30 @@
+﻿// Interface example
+public interface IAnimal
+{
+    void Speak();
+    void Eat();
+}
+
+
+public class Dog : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Bark!");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Eating dog food.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAnimal myDog = new Dog();
+        myDog.Speak();
+        myDog.Eat();   
+    }
+}
