ec8ca1eb00bdd793bddc5f4e3db22eb8a89dd99b
deepakkaleru <deepakkaleru@gmail.com>
2025-03-18 07:32:15 +0530

    Factorial

---
+++ Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class FactorialProgram
+{
+    static int Factorial(int num)
+    {
+        if (num == 0 || num == 1) return 1;
+        return num * Factorial(num - 1);
+    }
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int number =int.Parse(Console.ReadLine()); 
+
+        if (number < 0)
+        {
+            Console.WriteLine("Factorial is not defined for negative numbers.");
+        }
+        else
+        {
+            int result = Factorial(number);
+            Console.WriteLine($"Factorial of {number} is {result}");
+        }
+    }
+
+   
+}
+
