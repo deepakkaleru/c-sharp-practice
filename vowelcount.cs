62dcf02274e06d58b47f9049f41d9d33f81edfe6
deepakkaleru <deepakkaleru@gmail.com>
2025-03-18 06:52:28 +0530

    vowels count

---
+++ vowelcount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class VowelCount
+{
+    static int CountVowels(string input)
+    {
+        int count = 0;
+        foreach (char c in input.ToLower())
+        {
+            if ("aeiou".Contains(c)) // Check if the character is a vowel
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+        int vowelCount = CountVowels(input);
+        Console.WriteLine($"Number of vowels: {vowelCount}");
+    }
+}
+
