ee343842bc5ad5936309c26b9ae1ce568be2637a
deepakkaleru <deepakkaleru@gmail.com>
2025-03-24 22:29:26 +0530

    class and object example

---
+++ class-object.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Car
+    {
+        string model;
+        string color;
+        int year;
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Ford.model = "accent";
+            Ford.color = "red";
+            Ford.year = 1999;
+
+            Car maruti = new Car();
+            maruti.model = "nexa";
+            maruti.color = "white";
+            maruti.year = 2015;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(maruti.model);
+        }
+    }
+}
