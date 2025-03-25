c8d0aca35dfd37938e48a86f8d701c4536f42373
deepakkaleru <deepakkaleru@gmail.com>
2025-03-25 13:16:31 +0530

    class and object example

---
+++ constructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    // Constructor
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Person person = new Person("Deekshith", 25);
+
+       
+        person.Greet();
+    }
+}
